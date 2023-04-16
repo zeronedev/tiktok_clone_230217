183823458805c3d975e74a81355166307491a4d2
zeronedev <ok7375@gmail.com>
2023-04-16 22:51:32 +0900

    #4.9 Login Form:폼 여러입력 한번에 검사처리

--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone_230217/constants/gaps.dart';
 import 'package:tiktok_clone_230217/constants/sizes.dart';
+import 'package:tiktok_clone_230217/features/authentication/login_form_screen.dart';
 import 'package:tiktok_clone_230217/features/authentication/widgets/auth_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -11,15 +12,23 @@ class LoginScreen extends StatelessWidget {
     Navigator.of(context).pop();
   }
 
+  void _onEmailLoginTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const LoginFormScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
-          child: Column(children: const [
+          child: Column(children: [
             Gaps.v80,
-            Text(
+            const Text(
               'Log in to TikTok',
               style: TextStyle(
                 fontSize: Sizes.size24,
@@ -27,7 +36,7 @@ class LoginScreen extends StatelessWidget {
               ),
             ),
             Gaps.v20,
-            Text(
+            const Text(
               '계정 관리, 알림 확인, 동영상에 댓글 달기 등의 작업을 할 수 있습니다.',
               style: TextStyle(
                 fontSize: Sizes.size14,
@@ -36,12 +45,15 @@ class LoginScreen extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
             Gaps.v40,
-            AuthButton(
-              icon: FaIcon(FontAwesomeIcons.user),
-              text: '이메일과 비밀번호 사용',
+            GestureDetector(
+              onTap: () => _onEmailLoginTap(context),
+              child: const AuthButton(
+                icon: FaIcon(FontAwesomeIcons.user),
+                text: '이메일과 비밀번호 사용',
+              ),
             ),
             Gaps.v16,
-            AuthButton(
+            const AuthButton(
               icon: FaIcon(FontAwesomeIcons.apple),
               text: '애플 로그인',
             ),
