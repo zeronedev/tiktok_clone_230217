97aeb8f38368c17d7f8076432b85b730b09ea8ee
zeronedev <ok7375@gmail.com>
2023-06-03 14:11:28 +0900

    #5.3 Tutorial Screen: onboarding

---
+++ lib/features/onboarding/tutorial_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone_230217/constants/gaps.dart';
+import 'package:tiktok_clone_230217/constants/sizes.dart';
+
+class TutorialScreen extends StatefulWidget {
+  const TutorialScreen({super.key});
+
+  @override
+  State<TutorialScreen> createState() => _TutorialScreenState();
+}
+
+class _TutorialScreenState extends State<TutorialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        body: SafeArea(
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "멋진 비디오를 시청하세요!",
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "동영상은 보고, 좋아하고, 공유한 내용을 기반으로 개인화됩니다.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "규칙을 따르세요!",
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "동영상은 보고, 좋아하고, 공유한 내용을 기반으로 개인화됩니다.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Gaps.v52,
+                    Text(
+                      "보고 즐기세요!",
+                      style: TextStyle(
+                        fontSize: Sizes.size36,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Gaps.v16,
+                    Text(
+                      "동영상은 보고, 좋아하고, 공유한 내용을 기반으로 개인화됩니다.",
+                      style: TextStyle(
+                        fontSize: Sizes.size20,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size48,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                TabPageSelector(
+                  color: Colors.white,
+                  selectedColor: Colors.black38,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
