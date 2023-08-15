cc99bf9838123b4c4faa501905b1c3b64a342251
zeronedev <ok7375@gmail.com>
2023-08-16 00:26:32 +0900

    #7.3 Video Player: 비디오 플레이

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,15 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoPost extends StatefulWidget {
-  const VideoPost({super.key});
+  final Function onVideoFinished;
+  const VideoPost({super.key, required this.onVideoFinished});
 
   @override
   State<VideoPost> createState() => _VideoPostState();
 }
 
 class _VideoPostState extends State<VideoPost> {
+  final VideoPlayerController _videoPlayerController = // 비오컨레이어 컨트롤러를 생성한다.
+      VideoPlayerController.asset(
+    'assets/videos/video.mp4',
+  );
+
+  void _onVideoChange() {
+    if (_videoPlayerController.value.isInitialized) {
+      if (_videoPlayerController.value.duration ==
+          _videoPlayerController.value.position) {
+        widget.onVideoFinished();
+      }
+    }
+  }
+
+  void _initVideoPlayer() async {
+    // 1단계: 초기화 해줘야한다.
+    await _videoPlayerController.initialize();
+    _videoPlayerController.play();
+    setState(() {});
+    _videoPlayerController.addListener(_onVideoChange);
+  }
+
+  @override
+  void initState() {
+    // 한번만 실행된다
+    _initVideoPlayer();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(
+                    color: Colors.black,
+                  ))
+      ],
+    );
   }
 }
