28ba2b3e7e20e2ae0ac1089b043f44181dfac631
zeronedev <ok7375@gmail.com>
2023-08-16 03:12:32 +0900

    #7.4 VisibilityDetector: 가시성 감지기: 패키지 설치필요

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone_230217/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
-  const VideoPost({super.key, required this.onVideoFinished});
+  final int index;
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+    required this.index,
+  });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
@@ -27,7 +35,6 @@ class _VideoPostState extends State<VideoPost> {
   void _initVideoPlayer() async {
     // 1단계: 초기화 해줘야한다.
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
     setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
   }
@@ -45,17 +52,51 @@ class _VideoPostState extends State<VideoPost> {
     super.dispose();
   }
 
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
+  void _onTogglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+    } else {
+      _videoPlayerController.play();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
+    return VisibilityDetector(
+      key: Key('${widget.index}'),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
             child: _videoPlayerController.value.isInitialized
                 ? VideoPlayer(_videoPlayerController)
                 : Container(
                     color: Colors.black,
-                  ))
-      ],
+                  ),
+          ),
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: _onTogglePause,
+            ),
+          ),
+          const Positioned.fill(
+              child: IgnorePointer(
+            child: Center(
+              child: FaIcon(
+                FontAwesomeIcons.play,
+                color: Colors.white,
+                size: Sizes.size52,
+              ),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
