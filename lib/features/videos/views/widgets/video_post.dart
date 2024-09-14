cd645d1329303487e292751c299b31a85e26aa47
zeronedev <ok7375@gmail.com>
2024-09-14 17:17:49 +0900

    #22.3 Recap : ConsumerStatefulWidget a

--- lib/features/videos/views/widgets/video_post.dart
+++ lib/features/videos/views/widgets/video_post.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone_230217/constants/gaps.dart';
 import 'package:tiktok_clone_230217/constants/sizes.dart';
+import 'package:tiktok_clone_230217/features/videos/view_models/playback_config_vm.dart';
 import 'package:tiktok_clone_230217/features/videos/views/widgets/video_button.dart';
 import 'package:tiktok_clone_230217/features/videos/views/widgets/video_comments.dart';
 import 'package:tiktok_clone_230217/generated/l10n.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
-class VideoPost extends StatefulWidget {
+class VideoPost extends ConsumerStatefulWidget {
   final Function onVideoFinished;
   final int index;
   const VideoPost({
@@ -19,10 +21,10 @@ class VideoPost extends StatefulWidget {
   });
 
   @override
-  State<VideoPost> createState() => _VideoPostState();
+  VideoPostState createState() => VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost>
+class VideoPostState extends ConsumerState<VideoPost>
     with SingleTickerProviderStateMixin {
   late final VideoPlayerController _videoPlayerController; // 비오컨레이어 컨트롤러를 생성한다.
 
@@ -79,7 +81,9 @@ class _VideoPostState extends State<VideoPost>
 
   void _onPlaybackConfigChanged() {
     if (!mounted) return;
-    if (false) {
+    final muted = ref.read(playbackConfigProvider).muted;
+    ref.read(playbackConfigProvider.notifier).setMuted(!muted);
+    if (muted) {
       _videoPlayerController.setVolume(0);
     } else {
       _videoPlayerController.setVolume(1);
@@ -91,7 +95,7 @@ class _VideoPostState extends State<VideoPost>
     if (info.visibleFraction == 1 &&
         !_isPaused &&
         !_videoPlayerController.value.isPlaying) {
-      if (false) {
+      if (ref.read(playbackConfigProvider).autoplay) {
         _videoPlayerController.play();
       }
     }
@@ -173,11 +177,13 @@ class _VideoPostState extends State<VideoPost>
             left: 20,
             top: 40,
             child: IconButton(
-              icon: const FaIcon(false
+              icon: FaIcon(ref.watch(playbackConfigProvider).muted
                   ? FontAwesomeIcons.volumeOff
                   : FontAwesomeIcons.volumeHigh),
               color: Colors.white,
-              onPressed: () {},
+              onPressed: () {
+                _onPlaybackConfigChanged();
+              },
             ),
           ),
           const Positioned(
