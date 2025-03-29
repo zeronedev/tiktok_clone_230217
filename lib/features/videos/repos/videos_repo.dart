17391df1be8df5215b9f7fe82f18671ebcf1c263
zeronedev <ok7375@gmail.com>
2025-03-29 18:44:14 +0900

    #26.2 uploadVideoProvider:비디오파일 업로드

--- lib/features/videos/repos/videos_repo.dart
+++ lib/features/videos/repos/videos_repo.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone_230217/features/videos/models/video_model.dart';
 
 class VideosRepository {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
@@ -15,7 +16,10 @@ class VideosRepository {
         );
     return fileRef.putFile(video);
   }
-  // create a video document
+
+  Future<void> saveVideo(VideoModel data) async {
+    await _db.collection("videos").add(data.toJson());
+  }
 }
 
 final videosRepo = Provider((ref) => VideosRepository());
--- lib/features/videos/view_models/upload_video_view_model.dart
+++ lib/features/videos/view_models/upload_video_view_model.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tiktok_clone_230217/features/authentication/repos/authentication_repo.dart';
+import 'package:tiktok_clone_230217/features/users/view_models/users_view_model.dart';
+import 'package:tiktok_clone_230217/features/videos/models/video_model.dart';
 import 'package:tiktok_clone_230217/features/videos/repos/videos_repo.dart';
 
 class UploadVideoViewModel extends AsyncNotifier<void> {
@@ -13,11 +17,39 @@ class UploadVideoViewModel extends AsyncNotifier<void> {
     _repository = ref.read(videosRepo);
   }
 
-  Future<void> uploadVideo(File video) async {
+  Future<void> uploadVideo(File video, BuildContext context) async {
     final user = ref.read(authRepo).user;
-    state = const AsyncValue.loading();
-    state = await AsyncValue.guard(() async {
-      await _repository.uploadVideoFile(video, user!.uid);
-    });
+    final userProfile = ref.read(usersProvider).value;
+    debugPrint(userProfile.toString());
+    if (userProfile != null) {
+      state = const AsyncValue.loading();
+      state = await AsyncValue.guard(() async {
+        final task = await _repository.uploadVideoFile(
+          video,
+          user!.uid,
+        );
+        if (task.metadata != null) {
+          await _repository.saveVideo(
+            VideoModel(
+              title: "코드첼린지 타이틀",
+              description: "코드첼린지 설명",
+              fileUrl: await task.ref.getDownloadURL(),
+              thumbnailUrl: "",
+              creatorUid: user.uid,
+              likes: 0,
+              comments: 0,
+              createdAt: DateTime.now().millisecondsSinceEpoch,
+              creator: userProfile.name,
+            ),
+          );
+          if (!context.mounted) return;
+          context.pushReplacement("/home");
+        }
+      });
+    }
   }
 }
+
+final uploadVideoProvider = AsyncNotifierProvider<UploadVideoViewModel, void>(
+  () => UploadVideoViewModel(),
+);
