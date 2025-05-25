b00158a5959ee75905b6b474758a1e999852b6c0
zeronedev <ok7375@gmail.com>
2025-05-25 14:39:31 +0900

    #28.2 StreamProvider: 실시간 데이터변화

--- lib/features/inbox/view_models/messages_view_model.dart
+++ lib/features/inbox/view_models/messages_view_model.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tiktok_clone_230217/features/authentication/repos/authentication_repo.dart';
 import 'package:tiktok_clone_230217/features/inbox/models/message_model.dart';
@@ -20,6 +21,7 @@ class MessagesViewModel extends AsyncNotifier {
       final message = MessageModel(
         text: text,
         userId: user!.uid,
+        createdAt: DateTime.now().millisecondsSinceEpoch,
       );
       _repo.sendMessage(message);
     });
@@ -30,3 +32,23 @@ final messagesViewModelProvider =
     AsyncNotifierProvider<MessagesViewModel, void>(
   MessagesViewModel.new,
 );
+
+final chatProvider = StreamProvider<List<MessageModel>>((ref) {
+  final db = FirebaseFirestore.instance;
+
+  return db
+      .collection("chat_rooms")
+      .doc("4q7GNWJR8zYsPPfX9r8U")
+      .collection("texts")
+      .orderBy("createdAt")
+      .snapshots()
+      .map(
+        (event) => event.docs
+            .map(
+              (doc) => MessageModel.fromJson(
+                doc.data(),
+              ),
+            )
+            .toList(),
+      );
+});
